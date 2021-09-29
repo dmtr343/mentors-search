9bc18720062d866ffad69778a7fd030755028b8e
Dmitry <dmtr343@gmail.com>
2021-09-29 18:48:31 +0300

    created file structure of pages

--- src/router.js
+++ src/router.js
@@ -1,20 +1,27 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import MentorsList from './pages/mentors/MentorsList.vue';
+import MentorDetails from './pages/mentors/MentorDetails.vue';
+import MentorRegistration from './pages/mentors/MentorRegistration.vue';
+import MentorContact from './pages/mentors/MentorContact.vue';
+import RequestsRecieved from './pages/requests/RequestsRecieved.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/mentors' },
-        { path: '/mentors', component: null },
+        { path: '/mentors', component: MentorsList },
         {
             path: '/mentors/:id',
-            component: null,
+            component: MentorDetails,
             children: [
-                { path: '/contact', component: null },
+                { path: '/contact', component: MentorContact },
             ]
         },
-        { path: '/register', component: null },
-        { path: '/requests', component: null },
-        { path: '/:notFound(.*)', component: null },
+        { path: '/register', component: MentorRegistration },
+        { path: '/requests', component: RequestsRecieved },
+        { path: '/:notFound(.*)', component: NotFound },
     ],
 });
 
