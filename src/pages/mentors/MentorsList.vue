df1a59cd4dae78f1549e84f43d19814961c30b5f
Dmitry <dmtr343@gmail.com>
2021-09-30 20:39:23 +0300

    implemented MentorsList

--- src/pages/mentors/MentorsList.vue
+++ src/pages/mentors/MentorsList.vue
@@ -1,21 +1,41 @@
 <template>
   <section>Filter</section>
-  <section>List of mentors</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as a Mentor</router-link>
+    </div>
+    <ul v-if="hasMentors">
+      <li v-for="mentor in filteredMentors" :key="mentor.id">
+        {{ mentor.firstName }}
+      </li>
+    </ul>
+    <h3 v-else>No registered mentors yet.</h3>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    filteredMentors() {
+      return this.$store.getters['mentors/getMentors'];
+    },
+    hasMentors() {
+      return this.$store.getters['mentors/hasMentors'];
+    },
+  },
+};
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
 * {
   box-sizing: border-box;
 }
 
 html {
-  font-family: "Roboto", sans-serif;
+  font-family: 'Roboto', sans-serif;
 }
 
 body {
