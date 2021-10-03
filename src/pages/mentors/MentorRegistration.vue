1dfc963240007ab8644d60fa3e89f9666af913f2
Dmitry <dmtr343@gmail.com>
2021-10-03 12:27:12 +0300

    implemented MentorRegistration

--- src/pages/mentors/MentorRegistration.vue
+++ src/pages/mentors/MentorRegistration.vue
@@ -1,9 +1,17 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+      <h2>Register as a Mentor</h2>
+      <mentor-form></mentor-form>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {};
+import MentorForm from '../../components/mentors/MentorForm.vue';
+export default {
+  components: { MentorForm },
+};
 </script>
 
 <style>
