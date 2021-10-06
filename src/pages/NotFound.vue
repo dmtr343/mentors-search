3592963697674ea4bd28c570b984865e3fd3235b
Dmitry <dmtr343@gmail.com>
2021-10-06 19:02:50 +0300

    implemented NotFound page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+      <h2>404 - Page not found.</h2>
+      <p>
+        This page could not be found. But you still can check out
+        <router-link to="/mentors">all mentors page.</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
 
 <script>
